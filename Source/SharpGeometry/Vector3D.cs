bf7991ec31ba97f072ec5581683b8a38c8a3a06b
Patrick Stalph <patrick.stalph@gmx.de>
2016-02-19 21:33:35 +0100

    Create Solution

---
+++ Source/SharpGeometry/Vector3D.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpGeometry
+{
+    public struct Vector3D
+    {
+        public Vector3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public double X { get; }
+        public double Y { get; }
+        public double Z { get; }
+    }
+}
