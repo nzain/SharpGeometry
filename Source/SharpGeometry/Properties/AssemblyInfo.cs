499dac405895ed7c38625d131fbb5005265213e3
Patrick Stalph <patrick.stalph@gmx.de>
2016-03-02 23:17:03 +0100

    Bump version to 0.2

--- Source/SharpGeometry/Properties/AssemblyInfo.cs
+++ Source/SharpGeometry/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.Serialization;
 [assembly: AssemblyDescription("Provides geometry operations in 3D space. Vectors and matrices enable basic 3D algebra, but additional shapes (line, plane, etc.) are supported as well.")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("0.2.*")]
 
 // could use an url, if we had one :) linking github is wrong, so I'll go with an URN.
 [assembly: ContractNamespaceAttribute("urn:SharpGeometry", ClrNamespace = "SharpGeometry")]
